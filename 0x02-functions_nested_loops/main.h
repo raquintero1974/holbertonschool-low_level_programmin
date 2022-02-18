62b7fd63cbbd11a940f11d55f2f4123ec57efc95
raquintero1974 <raquintero1974@WIN-IS2O3L2J8QB.localdomain>
2022-02-17 21:19:27 -0500

    Adding files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar followed by a new linw
+ * Description: longer things about the main function
+ * Return: 0
+ */
+int main(void)
+{
+        putchar('_');
+        putchar('p');
+        putchar('u');
+        putchar('t');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+        return (0);
+}
+
+
