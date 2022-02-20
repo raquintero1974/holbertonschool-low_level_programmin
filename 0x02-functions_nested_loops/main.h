5bc3718a11065f5d9ab361564d57be4df5c49194
raquintero1974 <raquintero1974@WIN-IS2O3L2J8QB.localdomain>
2022-02-19 20:46:58 -0500

    Adding file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,25 @@
 #include <stdio.h>
-/**
- * main - Prints _putchar followed by a new linw
- * Description: longer things about the main function
- * Return: 0
- */
-int main(void)
-{
-        putchar('_');
-        putchar('p');
-        putchar('u');
-        putchar('t');
-        putchar('c');
-        putchar('h');
-        putchar('a');
-        putchar('r');
-        putchar('\n');
-        return (0);
-}
 
+int _putchar(char c);
 
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
