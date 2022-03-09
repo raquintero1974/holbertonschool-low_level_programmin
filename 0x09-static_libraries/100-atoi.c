86d924b3fd6fe9190060722bee144a97c9c94983
Raul Quintero <4638@holbertonschool.com>
2022-03-09 13:06:08 -0500

    Adding files

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to convert
+ *
+ * Return: result of conversion
+ */
+int _atoi(char *s);
+
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+#This is the README file for de project 0x09-static_libraries
