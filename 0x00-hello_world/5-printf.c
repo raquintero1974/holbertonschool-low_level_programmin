bfd683533bb1a9e68a3ebf21b476e46351051dd0
raquintero1974 <4638@holbertonschool.com>
2022-03-19 12:49:47 -0500

    Adding code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Eentry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
