510d9e6b7e6d03fa94ef4f365191e2f71ea86858
raquintero1974 <raquintero1974@WIN-IS2O3L2J8QB.localdomain>
2022-03-19 13:18:13 -0500

    Fixing code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Eentry point
+ * main - Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
