aa827f37ac0db94548807ae41106d775bb1fc8f4
Raul Quintero <4638@holbertonschool.com>
2022-05-14 23:28:25 -0500

    Adding file 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
